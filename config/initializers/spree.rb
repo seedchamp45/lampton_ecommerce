9680841046041cb93b4543e5b6cd30d14094bc89
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-01-06 16:49:54 +0700

    test

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -52,6 +52,7 @@ Spree.config do |config|
       access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       bucket:            ENV['S3_BUCKET_NAME']
+      s3_region: ENV['AWS_REGION']
     },
 
     storage:        :s3,
