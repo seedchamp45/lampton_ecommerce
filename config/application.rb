fcbd754635a0d549b0e87b8cf4515c28fb8cc589
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-01-27 23:07:18 +0700

    Revert to 56e05fced

--- config/application.rb
+++ config/application.rb
@@ -23,8 +23,6 @@ module LamptonEcommerce
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.enabled = true
-    config.serve_static_assets = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
