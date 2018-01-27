03208fc29d969a5edbc83d83a96299ae59cd36aa
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-01-28 02:28:25 +0700

    add config

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module LamptonEcommerce
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
     end
+    config.assets.initialize_on_precompile = true
     config.assets.precompile += ['index.css']
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
