52ca7809cb8504db4cce9e7533c854603de73c02
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-02-16 15:48:53 +0700

    add s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
-    :s3_region => ENV['AWS_REGION'],
+    :s3_region: 'ap-southeast-1'
     s3_credentials: {
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
