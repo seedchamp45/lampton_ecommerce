c0c005a70cf15c6b1b3e96dbfe870347322a30a3
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-03-22 22:10:54 +0700

    css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery-ui
 //= require rails-ujs
 //= require turbolinks
-//= require jquery-ui
-//= require jquery
 //= require jquery_ujs
 //= require_tree .
 //= require bootstrap
