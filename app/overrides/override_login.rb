181d68cad7c0cc81818db26f2a91cfaaf21bfb20
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-03-24 18:21:19 +0700

    ds

--- app/overrides/override_login.rb
+++ app/overrides/override_login.rb
@@ -1,5 +1,25 @@
 Deface::Override.new(:virtual_path => "spree/admin/orders/_shipment",
                      :name => "override_login",
                      :insert_top => ".shipment-number",
-                     :text => "<a href="">check transfer</a>-",
+                     :text => "
+                     
+            		
+                     <div style='disply:none;'<%= test = order.number %> 
+                     <%= photo = Photo.where(:title => test).first %></div>
+                     <br>
+                 
+                     <% if photo %>
+                     	SLIP IMAGE
+                     	      <br>
+                     	<%= link_to image_tag(photo.image.url, class: 'media-object'), photo.image.url, target: '_blank' %>
+              
+                     	 
+                     <% else %>
+                    	CUSTOMER DIDN'T UPLOAD SLIP YET
+                     <% end %>
+                     <br>
+
+
+
+                     ",
                      :disabled => false)
