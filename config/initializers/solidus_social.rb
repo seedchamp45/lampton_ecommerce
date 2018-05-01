b5faee7e62cbc6b100c7a9ed9bb147ae24958af4
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-05-01 17:58:46 +0700

    ds

--- config/initializers/solidus_social.rb
+++ config/initializers/solidus_social.rb
@@ -35,6 +35,14 @@ Spree::SocialConfig.configure do |config|
   }
 end
 
+if ActiveRecord::Base.connection.table_exists? 'spree_authentication_methods'
+  Spree::AuthenticationMethod.where(environment: ::Rails.env, provider: 'facebook').first_or_create do |auth_method|
+    auth_method.api_key = ENV['FACEBOOK_APP_ID']
+    auth_method.api_secret = ENV['FACEBOOK_APP_SECRET']
+    auth_method.active = true
+  end
+end
+
 SolidusSocial.init_providers
 
 OmniAuth.config.logger = Logger.new(STDOUT)
