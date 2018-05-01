7bddf9753b18fdda0c87e6e1e3cf1f829c5dfde0
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-05-02 01:08:45 +0700

    ds

--- config/initializers/solidus_social.rb
+++ config/initializers/solidus_social.rb
@@ -1,10 +1,12 @@
 if ActiveRecord::Base.connection.table_exists? 'spree_authentication_methods'
   Spree::AuthenticationMethod.where(environment: Rails.env, provider: 'facebook').first_or_create do |auth_method|
-    auth_method.api_key = ENV['FACEBOOK_APP_ID']
-    auth_method.api_secret = ENV['FACEBOOK_APP_SECRET']
+    auth_method.api_key = '1620076268100083'
+    auth_method.api_secret = '2a75ffd10172df3109a189cff0e05824'
     auth_method.active = true
   end
 end
+
+
 
 Spree::SocialConfig.configure do |config|
   config.use_static_preferences!
