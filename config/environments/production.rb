5fe0f50eb26fa59416f06b01bb1bfa08aac4f5ed
Kittipol Munkatitum <Kittipol.idas@gmail.com>
2018-06-20 22:40:54 +0700

    true asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.precompile =  ['*.js', '*.css', '*.css.erb'] 
   config.assets.precompile += ['index.css']
   config.assets.precompile += ['index.js']
